9cd73a29344533e73a72d008ddb7fda7b864f4ae
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-03-14 00:27:18 -0300

    Delete src directory

--- src/microservices/apiConfig.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-  Api_Url: "http://localhost:5000",
-  Mongo_Connection:
-    "mongodb+srv://geras:webmaster@url-shortener.h4hu2.mongodb.net/url-shortner?retryWrites=true&w=majority",
-};

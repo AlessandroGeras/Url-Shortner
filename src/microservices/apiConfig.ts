2ac06922d68c467fc90602a31037f7d6e925568b
Alessandro Geras <alessandrogeras@gmail.com>
2022-03-14 00:25:17 -0300

    Main Site

---
+++ src/microservices/apiConfig.ts
@@ -0,0 +1,5 @@
+export const config = {
+  Api_Url: "http://localhost:5000",
+  Mongo_Connection:
+    "mongodb+srv://geras:webmaster@url-shortener.h4hu2.mongodb.net/url-shortner?retryWrites=true&w=majority",
+};
